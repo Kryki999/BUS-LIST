d47f97faebf1b114a5c6c864e87762000de41187
kryki999 <gicekpl@gmail.com>
2017-05-14 22:37:10 +0200

    Create json and first code in js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-						
-						
+$(document).ready(function(){
+	
+	var Request = new XMLHttpRequest();
+	var url = 'dane.json';
+	
+	Request.open('GET', url);
+	Request.onload = function() {
+		var data = JSON.parse(Request.responseText);
+		console.log(data);
+	}
+
+	Request.send();
+});					
+
 						
 						
 			
