16ecf5d6fc6c8b7092d1ebe59f1731f8df6ee18b
kryki999 <gicekpl@gmail.com>
2017-05-15 20:51:10 +0200

    Complete 1 stop in js and complete dane.json

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 	
+	
+	
 	var Request = new XMLHttpRequest();
 	var url = 'dane.json';
 	
@@ -7,9 +9,81 @@ $(document).ready(function(){
 	Request.onload = function() {
 		var data = JSON.parse(Request.responseText);
 		console.log(data);
+	
+		var lasButton = document.getElementById('las-button');
+		var blaszakButton = document.getElementById('blaszak-button');
+		var zamekButton = document.getElementById('zamek-button');
+		var nadJaremButton = document.getElementById('21-button');
+		var actualStop = document.getElementById('stop');
+		
+		/*Funkcja wczytujaca domyslne ustawienia strony*/
+		
+		function myOnLoad(){
+			actualStop.innerHTML = "Międzylesie";
+			var allStops = document.getElementById('allStops');
+			
+			allStops.innerHTML = '<div class="line line1"><img class="small-image" src="image/bus.svg" alt="bus"><figcaption>Linia nr 3 </figcaption><ul id="listaLine3"></ul></div><div class="line line2"><img class="small-image" src="image/bus.svg" alt="bus"><figcaption>Linia nr 4 </figcaption><ul id="listaLine4"></ul></div><div class="line line3"><img class="small-image" src="image/bus.svg" alt="bus"><figcaption>Linia nr 12 </figcaption><ul id="listaLine12"></ul></div><div class="line line4"><img class="small-image" src="image/bus.svg" alt="bus"><figcaption>Linia nr 3(Weekendy i Święta) </figcaption><ul id="listaLine3W"></ul></div>';
+			
+			var line3 = document.getElementById('listaLine3');
+			var arr3 = data.miedzylesie.busNr3.kursy;
+			
+			for(var i in arr3)
+			$(line3).append('<li>'+arr3[i]+'</li>');
+			
+			
+			
+			var line4 = document.getElementById('listaLine4');
+			var arr4 = data.miedzylesie.busNr4.kursy;
+			
+			for(var i in arr4)
+			$(line4).append('<li>'+arr4[i]+'</li>');
+			
+			
+			
+			var line12 = document.getElementById('listaLine12');
+			var arr12 = data.miedzylesie.busNr12.kursy;
+			
+			for(var i in arr12)
+			$(line12).append('<li>'+arr12[i]+'</li>');
+			
+			
+			
+				
+			var line3W = document.getElementById('listaLine3W');
+			var arr3W = data.miedzylesie.busNr3W.kursy;
+			
+			for(var i in arr3W)
+			$(line3W).append('<li>'+arr3W[i]+'</li>');
+			
+		};
+		
+		myOnLoad();
+		
+		
+		
+		lasButton.onclick = function(){
+			myOnLoad();
+		};
+	
+		
+		blaszakButton.onclick = function(){ 		
+			actualStop.innerHTML = "Blaszak";
+													 
+													 
+		};
+		
+		zamekButton.onclick = function(){
+			actualStop.innerHTML = "Zamek";
+		};
+		
+		nadJaremButton.onclick = function(){
+			actualStop.innerHTML = "Osiedle Nad Jarem";
+		};
 	}
 
 	Request.send();
+	
+	
 });					
 
 						
